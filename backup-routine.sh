0f68ea0dc73f1114d0276a9cb857172991a15c46
Pedro Mendes <me@altart.top>
2024-02-07 22:36:58 -0300

    feat: Fix small error and make compression

--- backup-routine.sh
+++ backup-routine.sh
@@ -16,7 +16,8 @@ if [ "$(ssh "$SSH" 'ls -1 ./backups | wc -l')" -ge 10 ]; then
   # Print message to server that oldest backup is being removed
   sudo docker exec "$DOCKER_SERVER" rcon-cli 'tellraw @a [{"text":"[","color":"white"},{"text":"Server","color":"gold"},{"text":"] ","color":"white"},{"text":"Removing oldest backup","color":"yellow"}]'
   # Remove oldest backup
-  ssh "$SSH" 'rm ./backups/$(ssh $SSH "ls -1 ./backups | head -n 1")'
+  OLD_BACKUP=$(ssh "$SSH" 'ls -t1 ./backups | tail -n 1')
+  ssh "$SSH" rm "./backups/$OLD_BACKUP"
 fi
 
 # Create backup name with current date and time e.g. backup-2020-01-01_12-00-00
@@ -24,6 +25,10 @@ BACKUP_NAME="backup-$(date +%Y-%m-%d_%H-%M-%S)"
 
 # Use rsync to copy the server data to the backup directory on the host
 rsync -avz -e ssh -i /home/ubuntu/.ssh/id_rsa ./minecraft-data/ "$SSH:backups/$BACKUP_NAME"
+
+# Zip the backup and remove the original folder
+
+ssh "$SSH" "cd backups && tar -czf $BACKUP_NAME.tar.gz $BACKUP_NAME && rm -rf $BACKUP_NAME"
 
 # Enable saving again to allow the server to save properly
 sudo docker exec "$DOCKER_SERVER" rcon-cli save-on
